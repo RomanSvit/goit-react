3794f58375ca52b0d2d508c0a38ee9ce37b0dd53
Roman <roman.svitelskyi@gmail.com>
2020-02-02 21:50:32 +0200

    changes

--- React/hw-01/src/components/App.js
+++ React/hw-01/src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ProFile from './proFile/ProFile';
 import user from '../../src/user.json';
+import Statistic from './statistic/Statistic'
+import statistical from '../../src/statistical.json';
+import colors from '../../src/colors.json';
 
 const App = () => {
   return (
     <>
     <ProFile user={user}/>
-
+    <Statistic stats={statistical} title={"Roman"} colors={colors} />
     </>
   );
 }
--- React/hw-01/src/components/proFile/ProFile.js
+++ React/hw-01/src/components/proFile/ProFile.js
@@ -1,38 +1,50 @@
 import React from 'react';
 import css from './ProFile.module.css';
+import PropTypes from 'prop-types';
 
 const ProFile = ({user}) => {
     const {tag, name, location, stats, avatar} =user;
-    console.dir(stats)
+    // console.dir(stats)
     return (
         <div className={css.profile}>
-  <div className="description">
+  <div className={css.description}>
     <img
       src={avatar}
       alt={name}
       className={css.avatar}
     />
-<h2 className={css.name}>{name}</h2>
-<p className={css.tag}>@{tag}</p>
-    <p className={css.location}>{location} </p>
+    <h2 className={css.name}>{name}</h2>
+    <p className={css.tag}>@{tag}</p>
+    <p className={css.location}>{location}</p>
   </div>
 
   <ul className={css.stats}>
     <li>
       <span className={css.label}>Followers</span>
-      <span className={css.quantity}>{stats.Followers}</span>
+      <span className={css.quantity}>{stats.followers}</span>
     </li>
     <li>
       <span className={css.label}>Views</span>
-      <span className={css.quantity}>{stats.Views}</span>
+      <span className={css.quantity}>{stats.views}</span>
     </li>
     <li>
       <span className={css.label}>Likes</span>
-    <span className={css.quantity}>{stats.Likes}</span>
+      <span className={css.quantity}>{stats.likes}</span>
     </li>
   </ul>
 </div>
     );
 }
-
+// ProFile.defaultProps={
+//   avatar:'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
+// };
+ProFile.propTypes = {
+  user:PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.object.isRequired
+})
+};
 export default ProFile;
