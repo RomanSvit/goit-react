b78cb0e208b94eeabb1a688cc8395c768d71adc9
Roman <roman.svitelskyi@gmail.com>
2020-02-09 16:28:47 +0200

    changes

--- React/hw-02/phonebook/src/componenets/App.js
+++ React/hw-02/phonebook/src/componenets/App.js
@@ -6,17 +6,6 @@ import Form from "./form/Form";
 import Section from "./section/Section";
 import uuid from "uuid";
 
-// const CONTACTS = [
-//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" }
-// ];
-const handleFindContact = (contacts, filter) => {
-  return contacts.filter(elem =>
-    elem.name.toLowerCase().includes(filter.toLowerCase())
-  );
-};
 class App extends Component {
   state = {
     contacts: [
@@ -45,30 +34,44 @@ class App extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    const object = {
-      name: this.state.name,
-      number: this.state.number,
-      id: this.getId()
-    };
-    this.setState(prevstate => {
-      return {
-        contacts: [...prevstate.contacts, object],
-        name: "",
-        number: "",
-        id: ""
+    const hasContact = this.state.contacts.find(
+      elem => elem.name.toLowerCase() === e.target.name.value.toLowerCase()
+    );
+    if (!hasContact) {
+      const object = {
+        name: this.state.name,
+        number: this.state.number,
+        id: this.getId()
       };
-    });
+      this.setState(prevstate => {
+        return {
+          contacts: [...prevstate.contacts, object],
+          name: "",
+          number: "",
+          id: ""
+        };
+      });
+    } else {
+      alert(`${hasContact.name} is already in contacts!`);
+    }
   };
+
   handleDeleteConctact = e => {
     this.setState({
       contacts: this.state.contacts.filter(el => el.id !== e.target.id)
     });
   };
+
+  handleFindContact = (contacts, filter) => {
+    return contacts.filter(elem =>
+      elem.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
   render() {
-    console.log("object", this.state.contacts);
+    // console.log("object", this.state.contacts);
     const { contacts, filter } = this.state;
-    const filteredContacts = handleFindContact(contacts,filter);
-    console.log(filteredContacts);
+    const filteredContacts = this.handleFindContact(contacts, filter);
+    // console.log(filteredContacts);
     return (
       <>
         <Section title="Phonebook">
