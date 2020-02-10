186481bd29b1bb505a7a44b43d2908f28eb2e0e8
Roman <roman.svitelskyi@gmail.com>
2020-02-10 18:17:10 +0200

    Updates

---
+++ precache-manifest.66e770a6adf3cacc907bb96088f49dfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b0d528c10fbe82212d29f2bf749f03",
+    "url": "/goit-react/index.html"
+  },
+  {
+    "revision": "eb56979d0f1a715f492f",
+    "url": "/goit-react/static/js/2.4f5b8725.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react/static/js/2.4f5b8725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8492c9b3aaa21d994355",
+    "url": "/goit-react/static/js/main.2cf051aa.chunk.js"
+  },
+  {
+    "revision": "4a55f6324bd6c33d71e7",
+    "url": "/goit-react/static/js/runtime-main.06cb7be4.js"
+  }
+]);
