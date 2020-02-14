661c6616e5796d1c66d2fda2404d0eaa5e2d471e
Roman <roman.svitelskyi@gmail.com>
2020-02-14 14:42:04 +0200

    Updates

---
+++ precache-manifest.715d5bcfff7ea48ac1e134cf4b5885ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98147ca11272527c5d905d3315e4329",
+    "url": "/goit-react/React/hw-03/searchpic/index.html"
+  },
+  {
+    "revision": "f6f355fc91abf62c9a0d",
+    "url": "/goit-react/React/hw-03/searchpic/static/css/main.6a221095.chunk.css"
+  },
+  {
+    "revision": "7ff1277fa39825c5fc37",
+    "url": "/goit-react/React/hw-03/searchpic/static/js/2.54567c56.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react/React/hw-03/searchpic/static/js/2.54567c56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f355fc91abf62c9a0d",
+    "url": "/goit-react/React/hw-03/searchpic/static/js/main.59d1678f.chunk.js"
+  },
+  {
+    "revision": "8303e0b2484d8a77f0cd",
+    "url": "/goit-react/React/hw-03/searchpic/static/js/runtime-main.513e8d6f.js"
+  }
+]);
