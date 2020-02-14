993b7b240e5f811e793619380602571c96cb923b
Roman <roman.svitelskyi@gmail.com>
2020-02-14 14:43:35 +0200

    Updates

---
+++ precache-manifest.d965c0c242685d9660f24d157ed05a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119b84998eaaac1dc220953160c195ce",
+    "url": "/goit-react/index.html"
+  },
+  {
+    "revision": "f6f355fc91abf62c9a0d",
+    "url": "/goit-react/static/css/main.6a221095.chunk.css"
+  },
+  {
+    "revision": "7ff1277fa39825c5fc37",
+    "url": "/goit-react/static/js/2.54567c56.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react/static/js/2.54567c56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f355fc91abf62c9a0d",
+    "url": "/goit-react/static/js/main.59d1678f.chunk.js"
+  },
+  {
+    "revision": "ef771d813074ee3c5661",
+    "url": "/goit-react/static/js/runtime-main.b76a554c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react/React/hw-03/searchpic/precache-manifest.715d5bcfff7ea48ac1e134cf4b5885ff.js"
+  "/goit-react/precache-manifest.d965c0c242685d9660f24d157ed05a4e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react/React/hw-03/searchpic/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
